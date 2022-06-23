22fad78e9d5969fbc8db0acc63a6cda37b697051
Roberto Fortes <robertbernd23@gmail.com>
2022-06-23 16:23:58 -0300

    feat: run amplify init and amplify add auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "tutorialauthupload2d01dde4": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
